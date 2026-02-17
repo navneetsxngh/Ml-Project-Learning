8d06f89bfcf8b73de73571925aee7af22b027395
navneetsxngh <singhnavneet2587@gmail.com>
2026-02-17 12:12:57 +0530

    Exception and Logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_details(error: Exception, error_detail: sys) -> str:
@@ -38,11 +38,10 @@ class CustomException(Exception):
 
 
 # Testing block
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.INFO)
+if __name__ == "__main__":
 
-#     try:
-#         a = 1 / 0
-#     except Exception as e:
-#         logging.error("Exception occurred", exc_info=True)
-#         raise CustomException(e, sys) from None
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.error("Divide by Zero Error")
+        raise CustomException(e, sys)
--- src/logger1.py
+++ src/logger1.py
@@ -1,20 +1,21 @@
+## This is for different log files
 import logging
 import os
+from datetime import datetime
 
-# Create logs directory
+# Create 'logs' directory
 LOGS_DIR = os.path.join(os.getcwd(), "logs")
 os.makedirs(LOGS_DIR, exist_ok=True)
 
-# Fixed log file name
-LOG_FILE_PATH = os.path.join(LOGS_DIR, "log_file.log")
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Full file path
+LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)
 
 # Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    filemode="a",
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-# if __name__=="__main__":
-#     logging.info("Logging Has Started")
