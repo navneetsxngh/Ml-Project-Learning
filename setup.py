80b5d5ffe0cb88b31d377222edf353a1a2686a08
navneetsxngh <singhnavneet2587@gmail.com>
2026-02-17 09:13:19 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+# Read requirements.txt  Option 1
+def parse_requirements(filename):
+    '''
+    Docstring for parse_requirements
+
+    :param filename: Description
+    :type filename: str
+    :return: Description
+    :rtype: List[str]
+    '''
+    with open(filename, "r", encoding="utf-8") as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith("#")
+        ]
+
+# Read requirements.txt Option 2
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function reads requirements.txt and returns a clean list of packages.
+    It removes '-e .' which is only needed for local editable installs.
+    """
+    requirements = []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements if req.strip()]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="ML-project_Learning",
+    version="0.0.1",
+    author="Navneet Singh",
+    author_email="singhnavneet2587@gmail.com",
+    packages=find_packages(),
+    install_requires= get_requirements("requirements.txt"),
+    description="End-to-End ML Project for learning deployment and pipelines",
+    python_requires=">=3.8",
+)
