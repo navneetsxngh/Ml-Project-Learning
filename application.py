24aabc8e61420f22fb1af32fc6f8251416aa4646
navneetsxngh <singhnavneet2587@gmail.com>
2026-04-12 20:04:33 +0530

    Deployment Configuration

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
-
+application = Flask(__name__)
+app = application
 ## Route for a home page
 @app.route('/')
 def index():
@@ -37,4 +37,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
